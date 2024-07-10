fd10777ace465482e174622c5302293ae60b3529
Passawut <passawut.prinyagul@gmail.com>
2024-07-10 11:04:17 +0700

    feat: set up project

---
+++ chess-be/src/main/java/com/project/chess_be/ChessBeApplication.java
@@ -0,0 +1,13 @@
+package com.project.chess_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChessBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChessBeApplication.class, args);
+	}
+
+}
---
+++ chess-be/src/test/java/com/project/chess_be/ChessBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.chess_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChessBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
