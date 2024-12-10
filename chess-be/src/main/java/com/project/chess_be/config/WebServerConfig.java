6f5e16ef5f72f36ea7c64d01597a8cdce4a36eee
Passawut <passawut.prinyagul@gmail.com>
2024-12-10 19:27:14 +0700

    feat: update socket config

--- chess-be/src/main/java/com/project/chess_be/config/WebServerConfig.java
+++ chess-be/src/main/java/com/project/chess_be/config/WebServerConfig.java
@@ -1,13 +1,32 @@
 package com.project.chess_be.config;
+import com.project.chess_be.service.JwtService;
+import com.project.chess_be.service.MyUserDetailsService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.ChannelRegistration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.stomp.StompCommand;
+import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
+import org.springframework.messaging.support.ChannelInterceptor;
+import org.springframework.messaging.support.MessageHeaderAccessor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebServerConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    MyUserDetailsService MyUserDetailsService;
 
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry){
@@ -18,6 +37,35 @@ public class WebServerConfig implements WebSocketMessageBrokerConfigurer {
     public void configureMessageBroker(MessageBrokerRegistry registry){
         registry.setApplicationDestinationPrefixes("/app");
         registry.enableSimpleBroker("/game", "/queue", "/player");
+    }
+
+    @Override
+    public void configureClientInboundChannel(ChannelRegistration registration) {
+        registration.interceptors(new ChannelInterceptor() {
+            @Override
+            public Message<?> preSend(Message<?> message, MessageChannel channel) {
+                StompHeaderAccessor accessor =
+                        MessageHeaderAccessor.getAccessor(message, StompHeaderAccessor.class);
+
+                assert accessor != null;
+                if (StompCommand.CONNECT.equals(accessor.getCommand())) {
+
+                    String authorizationHeader = accessor.getFirstNativeHeader("Authorization");
+                    assert authorizationHeader != null;
+                    String token = authorizationHeader.substring(7);
+
+                    String username = jwtService.extractUserName(token);
+                    UserDetails userDetails = MyUserDetailsService.loadUserByUsername(username);
+                    UsernamePasswordAuthenticationToken usernamePasswordAuthenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                    SecurityContextHolder.getContext().setAuthentication(usernamePasswordAuthenticationToken);
+
+                    accessor.setUser(usernamePasswordAuthenticationToken);
+                }
+
+                return message;
+            }
+
+        });
     }
 
 }
